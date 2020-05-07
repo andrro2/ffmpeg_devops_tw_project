7b9732a8124b31220f794d63c0c8c66b3b7ac00e
András Rozner <roznernek@gmail.com>
2020-05-07 14:46:57 +0200

    Added test resources and added inotify for file monitoring

--- script.awk
+++ script.awk
@@ -119,4 +119,9 @@ END {
 
     write_shell_script(hierarchy)
 
+    print "inotifywait sources/ -e close_write | \n" \
+    "while read path action file; do \n"\
+    "    ARG=$file \n"\
+    "done \n" \
+    "./script.sh $ARG" >> output
 }
--- script.sh
+++ script.sh
@@ -2,57 +2,13 @@
 
 CHANGED_DEP=$1
 case $CHANGED_DEP in
-	intro)
-		echo "build intro -dep dependency4 -dep dependency5"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	content)
-		echo "build intro -dep dependency6 -dep dependency7"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	outro)
-		echo "build intro -dep dependency8 -dep dependency9"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency4)
-		echo "build intro -dep dependency4 -dep dependency5"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency5)
-		echo "build intro -dep dependency4 -dep dependency5"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency6)
-		echo "build intro -dep dependency6 -dep dependency7"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency7)
-		echo "build intro -dep dependency6 -dep dependency7"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency8)
-		echo "build intro -dep dependency8 -dep dependency9"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
-	dependency9)
-		echo "build intro -dep dependency8 -dep dependency9"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
-		;;
 	*)
-		echo "build intro -dep dependency4 -dep dependency5"
-		echo "build intro -dep dependency6 -dep dependency7"
-		echo "build intro -dep dependency8 -dep dependency9"
-		echo "build artifact -dep intro -dep content -dep outro"
-		echo "artifact step2"
+		ffmpeg -i sources/main.mp4 -i sources/logo.png -filter_complex "overlay=10:10" test1.mp4
 		;;
 esac
 
+inotifywait sources/ -e close_write | 
+while read path action file; do 
+    ARG=$file 
+done 
+./script.sh $ARG
